d3f6e7f998ed9d7b2d345b83cd2c01b9e67bd790
gjimenezd <135895559+gjimenezd@users.noreply.github.com>
2024-07-27 22:45:31 -0600

    Update language.ts

--- src/app/interfaces/language.ts
+++ src/app/interfaces/language.ts
@@ -12,5 +12,11 @@ export const Dictionary = [
     ["Spanish","Español"],
     ["Choose language","Escoger lenguaje"],
     ["Zoom","Ampliar"],
-    ["Size","Tamaño"],
+    ["Size", "Tamaño"],
+    ["Block", "Bloquear"],
+    ["Are you sure you want to block all cells?", "¿Estás seguro de que quieres bloquear todas las celdas?"],
+    ["row", "fila"],
+    ["column", "columna"],
+    ["There is no available space to place the word.", "No hay espacio disponible para colocar la palabra."],
+    ["The word is too long for this ", "La palabra es demasiado larga para esta "],
 ];

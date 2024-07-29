359121f833cc5bdb8210083c1145fa6e84e866bc
asalazarlCenfo <147350796+asalazarlCenfo@users.noreply.github.com>
2024-07-28 21:26:22 -0600

    Fix bug, update functions and new eventEmitter in compound-request,

--- src/app/components/chemcraft/compound-request/compound-request.component.ts
+++ src/app/components/chemcraft/compound-request/compound-request.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, effect, EventEmitter, inject, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ICompound } from '../../../interfaces';
 
@@ -14,13 +14,21 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
   public difficultyLevel: string = '';
   public compound: ICompound = {};
   public compoundName: string = '';
+  public compoundFormula: string = '';
   public lastCompound: string = '';
+  @Output() compoundGenerated = new EventEmitter<string>();
   public text: string = '';
   public textList: string[] = [];
 
+
   ngOnInit(): void {
-    console.log('ngOnInit called');
+
   }
+
+ //Cada que haya un cambio en la lista de compuestos, se genera una solicitud de compuesto
+ //Y como el unico cambio que se genera es el recibir la lista de compuestos generada en chemcraft
+ //y se actualiza la de compound-request, entonces es el unico cambio y genera la solicitud de compouesto
+ //con el Oninit no funciono porque la lista de compuestos estaria vacia
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['compoundsList'] && changes['compoundsList'].currentValue.length > 0) {
@@ -35,7 +43,6 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
     }
     const randomIndex = Math.floor(Math.random() * this.compoundsList.length);
     this.compound = this.compoundsList[randomIndex];
-    console.log(this.compound);
   }
 
   getRandomText(): string {
@@ -44,8 +51,11 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
   }
 
 
-   setDifficultyLevel(): void {
-    const uniqueElements = new Set(this.compound.formula?.replace(/[^A-Za-z]/g, '').split(''));
+  setDifficultyLevel(): void {
+    const formula = this.compound.formula || '';
+    const elementSymbolPar = /[A-Z][a-z]?/g; // esto funciona de manera que primero seleciona una letra mayuscula y luego una minuscula para elementos como el Al o el Fe, Mg, etc..
+    const elements = formula.match(elementSymbolPar) || []; //aca revisa que la formula tenga elementos y si no los tiene, devuelve un array vacio
+    const uniqueElements = new Set(elements);
     const elementCount = uniqueElements.size;
 
     if (elementCount <= 2) {
@@ -55,7 +65,7 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
     } else {
       this.difficultyLevel = 'Difícil';
     }
-  }
+}
 
 
   generateCompoundRequest(): void {
@@ -63,12 +73,15 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
     try {
     while (isSameCompound) {
       this.getRamdonCompound();
+      console.log(this.compound);
       if (this.compound.name !== this.lastCompound) {
         isSameCompound = false;
       }
     }
     console.log(this.compound.name);
     this.compoundName = this.compound.name!;
+    this.compoundFormula = this.compound.formula!;
+    this.compoundGenerated.emit(this.compoundFormula);
     this.textList = [
      'Debes de crear el compuesto "' + this.compoundName + '" ¿Que elementos necesitas?',
     'Recuerdas la fórmula quimíca del compuesto "' + this.compoundName + '" Genial, ¿De que elementos esta compuesta la fórmula?',
@@ -76,6 +89,7 @@ export class CompoundRequestComponent implements OnInit, OnChanges {
     ];
     this.text = this.getRandomText();
     this.setDifficultyLevel();
+    console.log(this.difficultyLevel);
     this.lastCompound = this.compound.name!;
   } catch (error) {
     console.error((error as Error).message);

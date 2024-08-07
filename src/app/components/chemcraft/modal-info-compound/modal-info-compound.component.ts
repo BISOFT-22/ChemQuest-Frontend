e48e000d8ed2dfd8e8e13da54a3ded6b1feaf9df
asalazarlCenfo <147350796+asalazarlCenfo@users.noreply.github.com>
2024-08-07 02:51:11 -0600

    Update modal-info-compound.component

--- src/app/components/chemcraft/modal-info-compound/modal-info-compound.component.ts
+++ src/app/components/chemcraft/modal-info-compound/modal-info-compound.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ICompound } from 'app/interfaces';
+import { ChemquestModalComponent } from "../../chemquest-modal/chemquest-modal.component";
 
 @Component({
   selector: 'app-modal-info-compound',
   standalone: true,
-  imports: [],
+  imports: [ChemquestModalComponent],
   templateUrl: './modal-info-compound.component.html',
   styleUrl: './modal-info-compound.component.scss'
 })
 export class ModalInfoCompoundComponent implements OnInit {
+  @Input() compound: ICompound = {};
+  @ViewChild('modal') modal!: ChemquestModalComponent;
+  @Output() action = new EventEmitter<{option: boolean}>();
 
-  constructor() { }
+  ngOnInit(): void {
+    console.log(this.compound);
+   }
 
-  ngOnInit(): void { }
+   sendAction(event: { option: boolean }): void {
+    this.action.emit({option: true});
+   }
 
 }
